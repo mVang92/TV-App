8c6f2f579047bd9aa95b92c62d5aac3a85023d1f
mVang <meng.vang92@yahoo.com>
2018-07-16 19:33:05 -0500

    Takes input

---
+++ cli.js
@@ -0,0 +1,13 @@
+// Write code to store the `<show-or-actor>` command line argument in a variable named `search`.
+var search = process.argv[2];
+
+// Store the `<actor-or-show-name>` command line argument in a variable named `term`.
+var term = process.argv.slice(3).join("+");
+
+// Keep in mind that this argument may contain spaces so it may span multiple indexes of `process.argv`.
+// Check out the [Array Slice Method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice)
+// to achieve this.
+
+// Then write an if/else statement to print to the console whether a show or actor is being searched for.
+
+console.log(search, + " " + term);
